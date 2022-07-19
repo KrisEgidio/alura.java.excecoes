775fa42438ef28b66ea11b32e6d9f0175b798d2f
KrisEgidio <krisegidio@gmail.com>
2022-07-19 08:55:13 -0300

    criando uma exceção e entendendo sobre hierarquia de exceções

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -2,19 +2,25 @@
 public class Fluxo {
 	public static void main(String[] args) {
         System.out.println("Ini do main");
-        metodo1();
+         try {
+        	 metodo1();	 
+         }catch(MinhaExcecao ex){
+        	 System.out.println(ex.getMessage());
+         }
+        
+        
         System.out.println("Fim do main");
     }
 
-    private static void metodo1() {
+    private static void metodo1() throws MinhaExcecao {
         System.out.println("Ini do metodo1");
         metodo2();
         System.out.println("Fim do metodo1");
     }
 
-    private static void metodo2() {
+    private static void metodo2() throws MinhaExcecao {
         System.out.println("Ini do metodo2");
-       /* for(int i = 1; i <= 5; i++) {
+       /*for(int i = 1; i <= 5; i++) {
             
         	System.out.println(i);
             
@@ -28,7 +34,7 @@ public class Fluxo {
         }*/
         
         //criação de uma exceção e jogando na pilha
-        throw new ArithmeticException("deu errado");
+       	throw new MinhaExcecao("deu errado");
         
         //System.out.println("Fim do metodo2");
     }
